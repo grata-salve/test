a51d304f0f1df7a9225faef69b6be98a95fd5aba
grata_salve <80597907+grata-salve@users.noreply.github.com>
2023-05-17 14:41:15 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Function<Function<String, Integer>, Predicate<String>> highOrderFunc =
+        strToInt -> str -> strToInt.apply(str) > 0;
+
+    Predicate<String> stringIntegerFunction = highOrderFunc.apply(Integer::parseInt);
+    System.out.println(stringIntegerFunction.test("-1231"));
+  }
+
+}
